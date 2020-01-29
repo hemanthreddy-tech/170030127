2017f133c878e09fc20fc95112305cd9b4fbf62b
hemanthreddy-tech <60381782+hemanthreddy-tech@users.noreply.github.com>
2020-01-29 10:44:24 +0530

    one2

---
+++ one3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class one3
+{
+public static void main(String args[])
+{
+int i=Integer.parseInt(args[0]);
+int j=Integer.parseInt(args[1]);
+int sum=i+j;
+System.out.print("The sum of "+ i +" and "+ j + "is " + sum);
+}
+}
